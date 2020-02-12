7fb45d1df8162b311237785269036c17114583e8
Cosmin Popovici <hellocosmin@gmail.com>
2020-02-12 18:41:13 +0200

    Use explicit Nunjucks extends.
    Required by inheritance. Can't use a default Layout anymore.

--- config.js
+++ config.js
@@ -319,7 +319,6 @@ module.exports = {
       path: 'build_local',
       extension: 'html',
     },
-    layout: 'src/layouts/default.njk',
     templates: {
       source: 'src/templates',
       filetypes: 'html|njk|nunjucks',
