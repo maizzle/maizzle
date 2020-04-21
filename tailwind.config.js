beed9049f7b2568defc03ba7af66033026bfb46b
Cosmin Popovici <hellocosmin@gmail.com>
2020-04-21 18:51:19 +0300

    feat(tailwind): update config with v1.3.0 options

--- tailwind.config.js
+++ tailwind.config.js
@@ -222,6 +222,8 @@ module.exports = {
       move: 'move',
       'not-allowed': 'not-allowed',
     },
+    divideColor: theme => theme('borderColor'),
+    divideWidth: theme => theme('borderWidth'),
     fill: {
       current: 'currentColor',
     },
@@ -423,6 +425,10 @@ module.exports = {
       '12': '12',
     },
     padding: theme => theme('spacing'),
+    space: (theme, { negative }) => ({
+      ...theme('spacing'),
+      ...negative(theme('spacing')),
+    }),
     stroke: {
       current: 'currentColor',
     },
@@ -648,6 +654,16 @@ module.exports = {
       '700': '700ms',
       '1000': '1000ms',
     },
+    transitionDelay: {
+      '75': '75ms',
+      '100': '100ms',
+      '150': '150ms',
+      '200': '200ms',
+      '300': '300ms',
+      '500': '500ms',
+      '700': '700ms',
+      '1000': '1000ms',
+    },
   },
   variants: {
     alignContent: ['responsive'],
@@ -668,6 +684,8 @@ module.exports = {
     boxSizing: ['responsive'],
     cursor: [],
     display: ['responsive'],
+    divideColor: ['responsive'],
+    divideWidth: ['responsive'],
     fill: [],
     flex: ['responsive'],
     flexDirection: ['responsive'],
@@ -704,6 +722,7 @@ module.exports = {
     pointerEvents: [],
     position: ['responsive'],
     resize: [],
+    space: ['responsive'],
     stroke: [],
     strokeWidth: [],
     tableLayout: ['responsive'],
@@ -737,10 +756,10 @@ module.exports = {
     transitionProperty: ['responsive'],
     transitionTimingFunction: ['responsive'],
     transitionDuration: ['responsive'],
+    transitionDelay: ['responsive'],
   },
   corePlugins: {
     accessibility: false,
-    container: false,
   },
   plugins: [],
 }
