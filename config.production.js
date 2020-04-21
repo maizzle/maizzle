f35a893cdefe27f6bae4ee36441f2a7b48a56d29
Cosmin Popovici <hellocosmin@gmail.com>
2020-04-21 17:57:33 +0300

    refactor(config): update production config

--- config.production.js
+++ config.production.js
@@ -3,9 +3,9 @@
 | Production config           https://maizzle.com/docs/environments/#production
 |-------------------------------------------------------------------------------
 |
-| This is were you define settings that optimize your emails for production.
+| This is where you define settings that optimize your emails for production.
 | These will be merged on top of the base config.js, so you only need to
-| specify the options that you are changing.
+| specify the options that are changing.
 |
 */
 
