9e4c24f074b5233546d6b1cdf889fc13fc126da0
Cosmin Popovici <hellocosmin@gmail.com>
2024-07-20 12:40:13 +0300

    chore: update config files
    add back comment blocks

--- config.js
+++ config.js
@@ -1,3 +1,13 @@
+/*
+|-------------------------------------------------------------------------------
+| Development config                      https://maizzle.com/docs/environments
+|-------------------------------------------------------------------------------
+|
+| This is the base configuration that Maizzle will use when you run commands
+| like `npm run build` or `npm run serve`. Additional config files will
+| inherit these settings, and can override them when necessary.
+*/
+
 /** @type {import('@maizzle/framework').Config} */
 export default {
   build: {
--- config.production.js
+++ config.production.js
@@ -1,3 +1,14 @@
+/*
+|-------------------------------------------------------------------------------
+| Production config                       https://maizzle.com/docs/environments
+|-------------------------------------------------------------------------------
+|
+| This is the production configuration that Maizzle will use when you run the
+| `npm run build` command. These settings will be merged on top of the base
+| `config.js`, so you only need to add the options that are changing.
+|
+*/
+
 /** @type {import('@maizzle/framework').Config} */
 export default {
   build: {
